a74f508eb4779d88ab8a4bcb8ae036bcf73051d9
skladkansaeng <settakarn.kl@gmail.com>
2024-05-06 00:52:22 +0700

    :bug: change isCompleted validate

--- src/todo-list/dto/update-todo-list.dto.ts
+++ src/todo-list/dto/update-todo-list.dto.ts
@@ -1,9 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateTodoListDto } from './create-todo-list.dto';
-import { IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateTodoListDto extends PartialType(CreateTodoListDto) {
-  @IsString()
+  @IsBoolean()
   @IsOptional()
   isCompleted?: boolean;
 }
